2c4c4a3548677834051e5862f3bb6d6e748c03d6
ADMIN <ADMIN@DESKTOP-DK4LEIH>
2023-07-18 14:15:57 +0530

    Book Store CRUD Operation Committed

---
+++ bookstore/src/main/java/com/Bookstore/Repository/MyBookRepository.java
@@ -0,0 +1,12 @@
+package com.Bookstore.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Bookstore.Entity.MyBookList;
+
+@Repository
+public interface MyBookRepository extends JpaRepository<MyBookList,Integer>
+{
+
+}
---
+++ bookstore/src/main/java/com/Bookstore/controller/MyBookListController.java
@@ -0,0 +1,25 @@
+package com.Bookstore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.Bookstore.Service.MyBookListService;
+
+@Controller
+public class MyBookListController
+{
+	@Autowired
+	private MyBookListService service;
+	
+					
+	@RequestMapping("/deleteMyList/{id}")
+	public String deleteMyList(@PathVariable(value="id") int id)
+	{
+		service.deleteById(id);
+		
+		return "redirect:/my_books";
+	}
+}
